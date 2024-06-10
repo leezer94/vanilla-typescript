d2f12cb31a054efef6bc8db39c6dbdbde72e2bd3
keonhee Lee <2kunhee94@gmail.com>
2024-06-10 19:34:10 +0900

    add take function that is able to take given lenth of indicies from iterable array

--- apps/live/src/pages/live-1/concurrent.ts
+++ apps/live/src/pages/live-1/concurrent.ts
@@ -37,10 +37,32 @@ async function concurrent<T>(limit: number, fs: (() => Promise<T>)[]) {
   return result;
 }
 
-export async function main(): Promise<void> {
+// 함수 지연상태를 외부에서 평가해서 조절 가능
+// generator function으로 만든 함수는 iterable 이기 떄문에 [...] 스프레드 문법으로 전개가 가능하다.
+
+function* take<T>(length: number, iterable: Iterable<T>) {
+  const iterator = iterable[Symbol.iterator]();
+
+  while (length-- > 0) {
+    const { value, done } = iterator.next();
+
+    if (done) break;
+
+    yield value;
+  }
+}
+
+async function concurrent2<T>(limit: number, fs: (() => Promise<T>)[]) {}
+
+export function main(): void {
   // 부하를 줄이고 싶을때
 
   console.time();
+
+  const iteratorNum: number[] = [...take(8, [1, 2, 3, 4, 5, 6, 7])];
+  const iteratorStr: string[] = [...take(8, ['가', '나', '다', '라'])];
+
+  console.log(iteratorNum, iteratorStr);
 
   // 비교 대상 (이전)
   // const files = await concurrent(3, [
@@ -52,19 +74,19 @@ export async function main(): Promise<void> {
   //   getFile('file1.ppt'),
   // ]);
 
-  const files: File[][] = await concurrent(3, [
-    () => getFile('file1.png'),
-    () => getFile('file2.jepg'),
-    () => getFile('file3.webp'),
-    () => getFile('file4.ppt'),
-    () => getFile('file5.ppt'),
-    () => getFile('file6.ppt'),
-    () => getFile('file7.ppt'),
-  ]);
+  // const files: File[][] = await concurrent(3, [
+  //   () => getFile('file1.png'),
+  //   () => getFile('file2.jepg'),
+  //   () => getFile('file3.webp'),
+  //   () => getFile('file4.ppt'),
+  //   () => getFile('file5.ppt'),
+  //   () => getFile('file6.ppt'),
+  //   () => getFile('file7.ppt'),
+  // ]);
 
   console.timeEnd();
 
   // flat을 사용하는케이스에 추가
   // 케이스는 많겠지만 동시성관리하면서 이차원배열을 일차원 배열로 만들어줄때
-  console.log(files.flat());
+  // console.log(files.flat());
 }
