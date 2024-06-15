e5797a208a23fa24ec96350cebc9ec23a7bb652d
keonhee Lee <2kunhee94@gmail.com>
2024-06-15 15:34:06 +0900

    lazy evaluation using concat from external lib

--- apps/live/src/pages/live-2/html.ts
+++ apps/live/src/pages/live-2/html.ts
@@ -1,4 +1,5 @@
 import { flat, map, pipe, reduce, zip } from '@fxts/core';
+import { escapeHtml } from './helper';
 
 // tagged template literal
 function upper(strings: TemplateStringsArray, ...values: string[]) {
@@ -7,13 +8,26 @@ function upper(strings: TemplateStringsArray, ...values: string[]) {
   return strings[0] + values[0].toUpperCase() + strings[1] + values[1].toUpperCase();
 }
 
-function html(strings: TemplateStringsArray, ...values: unknown[]) {
-  values.push('');
+// 키워드 지연평가 generator
+// 밸류 평가 시점까지 원본 유지 평가시점에 mutation 일어남
+// ~ 게 변할거다 라는 약속만 해놓은 객체 완성
+function* concat(...arrs) {
+  for (const arr of arrs) {
+    for (const a of arr) {
+      console.log('this console is called when iterator.next() is called');
+      yield* a;
+    }
+  }
+}
 
+function html(strings: TemplateStringsArray, ...values: unknown[]) {
   return pipe(
     zip(
       strings,
-      map((v) => `${v}`, values),
+      concat(
+        map((v) => escapeHtml(v), values),
+        [''],
+      ),
     ),
     flat,
     reduce((a, b) => a + b),
@@ -22,10 +36,14 @@ function html(strings: TemplateStringsArray, ...values: unknown[]) {
 
 export function main() {
   const a = 'a';
-  const b = 1;
-  const c = 'c';
+  const b = `<script></script>`;
+  const c = `<img onload=''>`;
 
-  const result = html`1${a}2${b}3${c}4`;
+  const result = html`<ul>
+    <li>${a}</li>
+    <li>${b}</li>
+    <li>${c}</li>
+  </ul>`;
 
   console.log(result, 'result');
 
